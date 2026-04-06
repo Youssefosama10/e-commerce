450eb029e2cf6a54cbd81dca50134003df94ab8c
youseef osama <youssefosama9458@gmail.com>
2026-04-06 19:04:17 +0200

    fix

--- src/app/utlis.ts
+++ src/app/utlis.ts
@@ -5,9 +5,11 @@ export async function userToken(): Promise< string | null >
 {
   const cookie = await cookies()
 
- const NextAuthToken = cookie.get("next-auth.session-token")?.value
-
+ let NextAuthToken = cookie.get("next-auth.session-token")?.value || cookie.get("__Secure-next-auth.session-token")?.value
+ 
  console.log(NextAuthToken);
+ 
+
  
 
 const jwtres = await decode( { secret: process.env.NEXTAUTH_SECRET! , token: NextAuthToken } )
